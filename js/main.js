a010583d5da6731879255556461ce17d7bbc6f4c
oJlerr <59744560+oJlerr@users.noreply.github.com>
2020-05-13 11:57:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("body").on('click', '[href*="#"]', function(e){
+		var fixed_offset ='';
+		$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 400);
+		e.preventDefault();
+	});
+});
